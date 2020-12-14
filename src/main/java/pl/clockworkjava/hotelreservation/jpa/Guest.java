fb470ce471cc2bc21294662b598373df88534338
Pawel Cwik <pawel@clockworkjava.pl>
2020-12-14 22:21:54 +0100

    First entity

---
+++ src/main/java/pl/clockworkjava/hotelreservation/jpa/Guest.java
@@ -0,0 +1,20 @@
+package pl.clockworkjava.hotelreservation.jpa;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private int age;
+
+    public Guest(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
